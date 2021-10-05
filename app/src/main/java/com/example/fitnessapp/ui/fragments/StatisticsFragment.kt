a01e427d3d7eacf8f656ac5ae238af677e3dac3f
Fedy Belaid <fedybelaid1902@gmail.com>
2021-10-05 11:04:40 +0100

    Added necessary fragments and layouts

---
+++ app/src/main/java/com/example/fitnessapp/ui/fragments/StatisticsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.fitnessapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fitnessapp.R
+
+class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
+}
