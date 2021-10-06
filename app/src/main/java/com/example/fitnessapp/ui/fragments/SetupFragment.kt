1db6f0b848797619281a6e2e3ba9db0374968de5
Fedy Belaid <fedybelaid1902@gmail.com>
2021-10-06 16:17:10 +0100

    Setting up navigation between fragments

--- app/src/main/java/com/example/fitnessapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/example/fitnessapp/ui/fragments/SetupFragment.kt
@@ -2,6 +2,23 @@ package com.example.fitnessapp.ui.fragments
 
 import androidx.fragment.app.Fragment
 import com.example.fitnessapp.R
+import kotlinx.android.synthetic.main.fragment_setup.*
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+
 
 class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+
+    }
+    
 }
